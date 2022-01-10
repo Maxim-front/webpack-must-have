be3ae9911238f6a59b30151400ea2cf31afc58a9
Maksim Karzhaneuski <maksim.karzhaneuski@itechart-group.com>
2022-01-10 16:05:43 +0300

    added dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  presets: [
-    "@babel/preset-react", // optional: react: this resolves react-files (jsx, tsx)
-    "@babel/preset-typescript", // allows  to use TypeScript
-    "@babel/preset-env", // compiles your js according with .browserslistrc
-  ],
-  plugins: [
-    "@babel/plugin-proposal-class-properties", // transforms static class properties as well as properties declared with the property initializer syntax
-    "jsx-classnames-advanced", // optional: react: this resolves className={object}
-  ],
+    presets: [
+        ["@babel/preset-react", { runtime: "automatic" }], // optional: react: this resolves react-files (jsx, tsx)
+        "@babel/preset-typescript", // allows  to use TypeScript
+        "@babel/preset-env", // compiles your js according with .browserslistrc
+    ],
+    plugins: [
+        "@babel/plugin-proposal-class-properties", // transforms static class properties as well as properties declared with the property initializer syntax
+        "jsx-classnames-advanced", // optional: react: this resolves className={object}
+    ],
 };
