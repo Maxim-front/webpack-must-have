f6dc71d65a43f3fc95bcc78788b0a14e0dac7e23
Maksim Karzhaneuski <maksim.karzhaneuski@itechart-group.com>
2022-01-17 13:55:21 +0300

    upgraded search bar

--- src/components/linksComponents/products/products.tsx
+++ src/components/linksComponents/products/products.tsx
@@ -2,7 +2,7 @@ import { RootState } from "@/components/store/reducers/store";
 import { getApiCardResourse } from "@/utils/network";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { API_PRODUCT_CARDS } from "../../../constants/api";
+import { API_PRODUCT_CARDS, API_SEARCH } from "../../../constants/api";
 import Gamecards from "../home/gameCards/gameCards";
 import Searchbar from "../home/searchBar/searchBar";
 import Filter from "./filter/filter";
@@ -27,6 +27,7 @@ const Products = ({ platform }: ProductsProps): JSX.Element => {
   const [items, setItems] = useState<Array<Card>>([]);
   const [isLoading, setIsLoading] = useState(false);
   const card = useSelector((state: RootState) => state.card);
+  // const dispatch = useDispatch();
 
   const initSortAndFilterProps = {
     genres: "all genres",
@@ -37,11 +38,14 @@ const Products = ({ platform }: ProductsProps): JSX.Element => {
 
   const [sortAndFilterProps, setSortAndFilterProps] = useState({ ...initSortAndFilterProps });
   const { genres, age, sortByCriteria, sortByType } = sortAndFilterProps;
+  const [urlTest, setUrlTest] = useState(API_SEARCH);
 
-  const getResponse = async (param: string) => {
+  const getResponse = async (url: string) => {
+    setUrlTest(url);
+
     setIsLoading(true);
     try {
-      const res = await getApiCardResourse<Array<Card>>(param);
+      const res = await getApiCardResourse<Array<Card>>(url);
       setItems(res);
       setIsLoading(false);
     } catch (error) {
@@ -56,7 +60,7 @@ const Products = ({ platform }: ProductsProps): JSX.Element => {
       );
     }
     if (age !== "all ages" && genres === "all genres") {
-      getResponse(`${API_PRODUCT_CARDS + platform}&age=${age}&_sort=date&_order=desc`);
+      getResponse(`${API_PRODUCT_CARDS + platform}&age=${age}&_sort=${sortByCriteria}&_order=${sortByType}`);
     }
     if (genres !== "all genres" && age === "all ages") {
       getResponse(`${API_PRODUCT_CARDS + platform}&genres=${genres}&_sort=${sortByCriteria}&_order=${sortByType}`);
@@ -67,14 +71,14 @@ const Products = ({ platform }: ProductsProps): JSX.Element => {
   }, [platform, sortAndFilterProps, card]);
 
   const onChange = (value: string, name?: string) => {
-    if (name) setSortAndFilterProps({ ...initSortAndFilterProps, [name]: value });
+    if (name) setSortAndFilterProps({ ...sortAndFilterProps, [name]: value });
   };
 
   return (
     <div className={styles.products_page}>
       <Filter onchange={onChange} platform={platform} />
       <div className={styles.right_components}>
-        <Searchbar />
+        <Searchbar urlTest={urlTest} />
         {isLoading ? (
           <img src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif" alt="" className={styles.loader} />
         ) : (
